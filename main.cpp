2a90e1257305720d194c9b6f9fa043708640f168
Lin Jie <3150470846@qq.com>
2024-10-04 20:38:13 +0800

    完成作业

--- main.cpp
+++ main.cpp
@@ -3,5 +3,25 @@
 // 比较三个数的大小
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
-    // 在这里完成你的代码
+    scanf("%d,%d,%d", &a, &b, &c);
+	int t;
+	if (a > b){
+		t = a;
+		a = b;
+		b = t;
+	}
+	if (a > c) {
+		t = a;
+		a = c;
+		c = t;
+	}
+	if (b > c) {
+		t = b;
+		b = c;
+		c = t;
+	}
+	result.min=c;
+    result.mid=b;
+    result.max=a;
+		printf("max=%d,mid=%d,min=%d\n", &a, &b, &c);
 }
